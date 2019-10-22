4829a69025aaff6b13ca26f98a2e31d38322b657
Cody Dunkley <redstar40k@gmail.com>
2019-10-21 22:59:43 -0500

    browsing functionality

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NewsService } from 'src/app/services/news.service'
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,15 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor( private newsService: NewsService ) {}
+
+  ngOnInit() {
+    // this.newsService.fetchNews().subscribe();
+  }
+
+  ionViewWillEnter() {
+    this.newsService.fetchNews().subscribe();
+  }
 
 }
+
